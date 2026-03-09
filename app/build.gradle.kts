cdd3e0e2f9d9bd0031ac32e6cd71782432170404
Simon Schäffner <simon.schaeffner@googlemail.com>
2026-03-09 22:06:39 +0100

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ version = "0.0.0-SNAPSHOT"
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "2.3.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.3.2"
     kotlin("plugin.serialization") version "2.3.0"
     application
 }
